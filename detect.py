8adcb37368ba2574c4b664fec91a6f6440783ad1
Kentaro Mitsuyasu <kmwebnet@gmail.com>
2020-03-16 17:52:26 +0900

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,8 @@
+import pyaudio as pa
+
+if __name__ == "__main__":
+
+    p_in = pa.PyAudio()
+    print ("device num: {0}".format(p_in.get_device_count()))
+    for i in range(p_in.get_device_count()):
+        print (p_in.get_device_info_by_index(i))
